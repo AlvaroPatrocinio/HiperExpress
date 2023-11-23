785c32bc2ab0fd3f83d6f276d3b0ba87027ec0c5
AlvaroPatrocinio <143203961+AlvaroPatrocinio@users.noreply.github.com>
2023-11-23 09:47:10 -0300

    Delete src directory

--- src/Cliente.java
+++
@@ -1,8 +0,0 @@
-//Aqui estamos definindo a interface para respresentar a abstração 'Cliente'.
-
-public interface Cliente {
-
-    //Possuimos um método exibir informações para listar as informações de determinado cliente.
-    void exibirInformacoes();
-
-}
--- src/TipoCliente.java
+++
@@ -1,8 +0,0 @@
-//Aqui estamos definindo a interface para respresentar a implementação 'Tipo Cliente'.
-
-public interface TipoCliente {
-
-    //Possuimos um método obter tipo cliente para obter o tipo de cliente.
-    String obterTipoCliente();
-
-}
