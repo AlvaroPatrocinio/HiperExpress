a7b2abb284126ffa4d401eec127dec46ce2db43b
AlvaroPatrocinio <143203961+AlvaroPatrocinio@users.noreply.github.com>
2023-11-23 09:42:18 -0300

    Primeiro Commit

---
+++ HiperExpress/src/main/java/com/mycompany/hiperexpress/HiperExpress.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hiperexpress;
+
+/**
+ *
+ * @author OMC
+ */
+public class HiperExpress {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
