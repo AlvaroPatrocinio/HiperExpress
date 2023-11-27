d72b5652360d1bfff8e1de2e6fb01e4ee82dbdd9
AlvaroPatrocinio <143203961+AlvaroPatrocinio@users.noreply.github.com>
2023-11-27 16:23:40 -0300

    Instanciando na main

--- HiperExpress/src/main/java/com/mycompany/hiperexpress/HiperExpress.java
+++ HiperExpress/src/main/java/com/mycompany/hiperexpress/HiperExpress.java
@@ -1,84 +1,25 @@
 package com.mycompany.hiperexpress;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-
-
-
+        
 public class HiperExpress {
     
-    private static Scanner input = new Scanner(System.in);
-    private static Scanner input2 = new Scanner(System.in);
-    private static ArrayList<Produto> produtos;
-    private static List<Caixa> caixas;
+private static ArrayList<Produto> produtos;
 
-     static {
-        // Inicialização da lista de caixas
-        caixas = new ArrayList<>();
-        caixas.add(new Caixa(1, "Rápida"));
-        caixas.add(new Caixa(2, "Preferencial"));
-        caixas.add(new Caixa(3, "Normal"));
-        caixas.add(new Caixa(4, "Normal"));
-        caixas.add(new Caixa(5, "Normal"));
-    }
+private static List<Caixa> caixas;
 
-    // Acessar os caixas
-    public static List<Caixa> getCaixas() {
-        return caixas;
-    }
-    
-    private static void menuadm(){
+private Estoque estoque = new Estoque();
         
-         System.out.println("----------------------------------------------");
-        System.out.println("-----------Bem vindo ao HiperEXPRESS----------");
-        System.out.println("----------------------------------------------");
-        System.out.println("**********Selecione a Opção desejada**********");
-        System.out.println("----------------------------------------------");
-        System.out.println("/    Opção 1 - Cadastrar de Produto     /");
-        System.out.println("/    Opção 2 - Listar     /");
-        System.out.println("/    Opção 3 - Comprar     /");
-        System.out.println("/    Opção 4 - Carrinho     /");
-        System.out.println("/    Opção 5 - Sair     /");
         
-        int option = input.nextInt();
-        switch (option){
-            default:
-                System.out.println("Opção Inválida!");
-                
-            case 1:
-                System.out.println("Digite o nome do Produto: ");
-            String Produtonome = input2.nextLine();
-                System.out.println("Digite o preço do Produto: ");
-            Double Produtopreco = input.nextDouble();
-                System.out.println("Digite a Categoria Desejada: ");
-                System.out.println("HortiFruti\nAlimentos \nCongelados \nBebidas");
-             String ProdutoCategoria  = input2.nextLine();
-              Produto produto1 = new Produto(Produtonome, Produtopreco, ProdutoCategoria);   
-              produtos.add(produto1);
-              Estoque estoque = new Estoque();
-                System.out.println("Digite a quantidade a dar entrada no estoque: ");
-                int Produtoestoque = input.nextInt();
-                estoque.adicionarProduto(produto1, Produtoestoque);
-                System.out.println("Produto Adicionado!"); 
-        }
+public static void main(String[] args) {
+       
+       Sistema sistema = Sistema.getInstance();
+       sistema.menuadm();
+       
+
         
 
-        
-    }
-
-    
-
-    public static void main(String[] args) {
-        produtos = new ArrayList<>();
-        menuadm(); 
-        /* 
-        //Criar produto
-        Produto produto1 = new Produto("Arroz", 1.5, "Alimento");
-        Produto produto2 = new Produto("Feijão", 2.0, "Alimento");
-
-        // Criação do estoque
-        Estoque estoque = new Estoque();
-        
+        /*
         // Adicionar produtos
         estoque.adicionarProduto(produto1, 50);
         estoque.adicionarProduto(produto2, 30);
@@ -110,9 +51,7 @@ public class HiperExpress {
         for (Caixa caixa : testcaixas) {
             System.out.println(caixa);
         } */
-
-    }
     
+    
+    }
 }
-
-
